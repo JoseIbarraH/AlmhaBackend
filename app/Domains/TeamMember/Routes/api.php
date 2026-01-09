461564dd10980c5a09cab38fd9c9fd0445793ed3
Jose Carlos Ibarra Herrera <jcibarrah1423@gmail.com>
2026-01-09 16:54:31 -0500

    add permission control to route

--- app/Domains/TeamMember/Routes/api.php
+++ app/Domains/TeamMember/Routes/api.php
@@ -5,12 +5,12 @@ use App\Domains\TeamMember\Controllers\TeamMemberController;
 use App\Domains\TeamMember\Controllers\TeamMemberContentController;
 
 
-Route::prefix('team_member')->controller(TeamMemberController::class)->group(function () {
+Route::middleware(['auth:sanctum', 'permission.map'])->prefix('team_member')->controller(TeamMemberController::class)->group(function () {
     Route::get('/', 'list_teamMember');
     Route::get('/{id}', 'get_teamMember');
 });
 
-Route::prefix('team_member')->controller(TeamMemberContentController::class)->group(function () {
+Route::middleware(['auth:sanctum', 'permission.map'])->prefix('team_member')->controller(TeamMemberContentController::class)->group(function () {
     Route::post("/", 'create_teamMember');
     Route::patch("/{id}", 'update_teamMember');
     Route::delete("/{id}", 'delete_teamMember');
